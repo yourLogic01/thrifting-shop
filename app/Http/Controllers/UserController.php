a47491b1f2b965e52429b55e2e5b3e2febe1d20b
yourLogic01 <2010631170004@student.unsika.ac.id>
2023-11-21 20:00:25 +0700

    feat : user delete function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -96,8 +96,11 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        toast("User Deleted", 'success');
+        return redirect()->route('user.index');
     }
 }
