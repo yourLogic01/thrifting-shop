7ed83544f0525f97d4eab2200052a8a9be2a0578
yourLogic01 <2010631170004@student.unsika.ac.id>
2023-11-28 13:10:19 +0700

    feat : adding gate between owner and employee

--- routes/web.php
+++ routes/web.php
@@ -110,7 +110,7 @@ Route::resource('users', UserController::class)->except('show')->names([
   'edit' => 'user.edit',
   'update' => 'user.update',
   'destroy' => 'user.destroy',
-]);
+])->middleware('owner');
 
 // User Profile Route
 Route::get('user-profile', [ProfileController::class, 'editProfile'])->name('profile.index');
